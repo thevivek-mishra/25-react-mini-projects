901ada89476e95b301ce3c7b4bd7fcca66920069
vivek kumar mishra <vivekmishra4701@gmail.com>
2024-08-01 23:47:45 +0530

    project 1: Accordian

---
+++ mini-projects/src/components/accordian/data.js
@@ -0,0 +1,24 @@
+const data = [
+    {
+        id:'1',
+        question: "What is jsx",
+        answer:"jsx is the extension of js  "
+    },
+    {
+        id: 2,
+        question: "what is prop drilling",
+        answer: "passsing data from one component to another component is known as prop drilling"
+    },
+    {
+        id:3,
+        question: "what is redux",
+        answer: "redux is js libaray  which is used for centralize data storage"
+    },
+    {
+        id:4,
+        question:"what is key in react",
+        answer: "key in react  is used to uniquely identify the list "
+    }
+]
+
+export default data;
